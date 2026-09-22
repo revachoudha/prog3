ca96c7f088d6bbc5ff4f92d6b100c75e3da83fa6
Reva Choudha <revachoudha@gmail.com>
2026-09-22 12:32:33 -0500

    notes abt logic

--- src/assignment/Interpreter.java
+++ src/assignment/Interpreter.java
@@ -1,6 +1,7 @@
 package assignment;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Responsible for loading critter species from text files and interpreting the
@@ -12,12 +13,66 @@ import java.io.IOException;
 public class Interpreter implements CritterInterpreter {
 
 	public void executeCritter(Critter c) {
-		// obviously, your code should do something
+		// loop through the ArrayList using .getNextCodeLine()
+		// check if currentLine is out of bounds
+		// break up each String line to a String array (to separate the instruction from
+		// the line number)
+		// hella if-else statements?:
+		// ACTIONS
+		// if parts[0] = hop: critter.hop(); crotter.setNextCodeLine(currentLine+1);
+		// return;
+		// left
+		// right
+		// eat
+		// infect - maybe do a nested if else here - if length of the array is 2, call
+		// lineJump
+
+		// JUMPS
+		// go
+		// ifempty
+		// ifhungry
+		// ifstarving
+		// ifrandom
+		// ifangle
+		// ifwall
+		// ifenemy
+		// ifally
+		// ifempty
+
+		// REGISTERS
+		// write
+		// add
+		// sub
+		// inc
+		// dec
+		// iflt
+		// ifeq
+		// ifgt
+
 		return;
 	}
 
 	public CritterSpecies loadSpecies(String filename) throws IOException {
-		// obviously, your code should do something
-		return null;
+		String name;
+		ArrayList<String> code;
+		// read the file using BufferedReader .readLine()
+		// first line is species name
+		// iterate through all instructions, save them to an ArrayList<String>
+		// exit at the blank line
+
+		return new CritterSpecies(name, code);
+	}
+
+	// this is a helper method that will translate from the given target line
+	// notation for go to the line number
+	public int lineJump(String target, int currentLine, Critter cri) {
+		int nextLine;
+		// 4 cases:
+		// n is preceeded by +
+		// n is preceeded by -
+		// n has no prefix: absolute jump to n
+		// n is a register number (preceded by "r"): absolute jump to the register
+		// number
+		return nextLine;
 	}
 }
