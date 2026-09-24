8fa41e07d770ec18ced57f68cef1db295156bf0b
Prithviraj <prithvirajchauhan0650@gmail.com>
2026-09-24 01:26:58 -0500

    Debugged Interpreter and optimized program efficiency for various edge cases

--- src/assignment/Interpreter.java
+++ src/assignment/Interpreter.java
@@ -13,158 +13,168 @@ import java.io.*;
 public class Interpreter implements CritterInterpreter {
 
 	public void executeCritter(Critter c) {
-		ArrayList<String> codeList = (ArrayList<String>) c.getCode();
-		Random rand = new Random();
+		List<String> codeList = c.getCode();
+		int currentLine = c.getNextCodeLine();
 
 		while (true) {
-			int currentLine = c.getNextCodeLine();
-			String[] parts = codeList.get(currentLine).split(" ");
-			String action = parts[0];
-			int n = 0;
-
-			if (action.equals("hop")) {
-				c.hop();
-				c.setNextCodeLine(currentLine + 1);
-			} else if (action.equals("left")) {
-				c.left();
-				c.setNextCodeLine(currentLine + 1);
-				return;
-			} else if (action.equals("right")) {
-				c.right();
-				c.setNextCodeLine(currentLine + 1);
-				return;
-			} else if (action.equals("eat")) {
-				c.eat();
-				c.setNextCodeLine(currentLine + 1);
+			if(currentLine < 1 || currentLine >= codeList.size()) {
 				return;
 			}
+			try {
+				String[] parts = codeList.get(currentLine).trim().split("\\s+");
+				String action = parts[0];
 
-			// line jumps
-			else if (action.equals("go")) {
-				currentLine = lineJump(parts[1], currentLine, c);
-			} else if (action.equals("ifempty")) {
-				int bearing = Integer.parseInt(parts[1]);
-				if (c.getCellContent(bearing) == Critter.EMPTY) {
-					currentLine = lineJump(parts[2], currentLine, c);
-				} else {
-					currentLine++;
+				if (action.equals("hop")) {
+					c.hop();
+					c.setNextCodeLine(currentLine + 1);
+					return;
+				} else if (action.equals("left")) {
+					c.left();
+					c.setNextCodeLine(currentLine + 1);
+					return;
+				} else if (action.equals("right")) {
+					c.right();
+					c.setNextCodeLine(currentLine + 1);
+					return;
+				} else if (action.equals("eat")) {
+					c.eat();
+					c.setNextCodeLine(currentLine + 1);
+					return;
 				}
-			} else if (action.equals("ifhungry")) {
-				if (c.getHungerLevel() == Critter.HungerLevel.HUNGRY
-						|| c.getHungerLevel() == Critter.HungerLevel.STARVING) {
-					currentLine = lineJump(parts[1], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			} else if (action.equals("ifstarving")) {
-				if (c.getHungerLevel() == Critter.HungerLevel.STARVING) {
-					currentLine = lineJump(parts[1], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			} else if (action.equals("ifrandom")) {
-				if (rand.nextBoolean()) {
-					currentLine = lineJump(parts[1], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			} else if (action.equals("ifangle")) {
-				int b1 = Integer.parseInt(parts[1]);
-				int b2 = Integer.parseInt(parts[2]);
 
-				if (c.getOffAngle(b1) == b2) {
-					currentLine = lineJump(parts[3], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			} else if (action.equals("ifwall")) {
-				int bearing = Integer.parseInt(parts[1]);
-				if (c.getCellContent(bearing) == Critter.WALL) {
-					currentLine = lineJump(parts[2], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			} else if (action.equals("ifenemy")) {
-				int bearing = Integer.parseInt(parts[1]);
-				if (c.getCellContent(bearing) == Critter.ENEMY) {
-					currentLine = lineJump(parts[2], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			} else if (action.equals("ifally")) {
-				int bearing = Integer.parseInt(parts[1]);
-				if (c.getCellContent(bearing) == Critter.ALLY) {
-					currentLine = lineJump(parts[2], currentLine, c);
-				} else {
-					currentLine++;
-				}
-			}
-			// Register methods start here
-			else if (action.equals("infect")) {
-				if (parts.length == 1) {
-					c.infect();
-				} else if (parts.length > 1) {
-					try {
-						n = Integer.parseInt(parts[1]);
-					} catch (NumberFormatException e) {
-						System.err.println("Parameter for infect method was not an int");
+				// line jumps
+				else if (action.equals("go")) {
+					currentLine = lineJump(parts[1], currentLine, c);
+				} else if (action.equals("ifempty")) {
+					int bearing = Integer.parseInt(parts[1]);
+					if (c.getCellContent(bearing) == Critter.EMPTY) {
+						currentLine = lineJump(parts[2], currentLine, c);
+					} else {
+						currentLine++;
 					}
-					c.infect(n);
-				}
-			} else if (action.equals("write")) {
-				int reg = Integer.parseInt(parts[1].substring(1));
-				int value = Integer.parseInt(parts[2]);
-				c.setReg(reg, value);
-				currentLine++;
-			} else if (action.equals("add")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				int r2 = Integer.parseInt(parts[2].substring(1));
-				c.setReg(r1, c.getReg(r1) + c.getReg(r2));
-				currentLine++;
-			} else if (action.equals("sub")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				int r2 = Integer.parseInt(parts[2].substring(1));
-				c.setReg(r1, c.getReg(r1) - c.getReg(r2));
-				currentLine++;
+				} else if (action.equals("ifhungry")) {
+					if (c.getHungerLevel() == Critter.HungerLevel.HUNGRY
+							|| c.getHungerLevel() == Critter.HungerLevel.STARVING) {
+						currentLine = lineJump(parts[1], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifstarving")) {
+					if (c.getHungerLevel() == Critter.HungerLevel.STARVING) {
+						currentLine = lineJump(parts[1], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifrandom")) {
+					if (c.ifRandom()) {
+						currentLine = lineJump(parts[1], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifangle")) {
+					int b1 = Integer.parseInt(parts[1]);
+					int b2 = Integer.parseInt(parts[2]);
 
-			} else if (action.equals("inc")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				c.setReg(r1, c.getReg(r1) + 1);
-				currentLine++;
-			} else if (action.equals("dec")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				c.setReg(r1, c.getReg(r1) - 1);
-				currentLine++;
-			} else if (action.equals("iflt")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				int r2 = Integer.parseInt(parts[2].substring(1));
-				int nth = Integer.parseInt((parts[3].substring(1)));
-				if (c.getReg(r1) < c.getReg(r2)) {
-					currentLine = nth;
-				} else {
-					currentLine++;
+					if (c.getOffAngle(b1) == b2) {
+						currentLine = lineJump(parts[3], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifwall")) {
+					int bearing = Integer.parseInt(parts[1]);
+					if (c.getCellContent(bearing) == Critter.WALL) {
+						currentLine = lineJump(parts[2], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifenemy")) {
+					int bearing = Integer.parseInt(parts[1]);
+					if (c.getCellContent(bearing) == Critter.ENEMY) {
+						currentLine = lineJump(parts[2], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifally")) {
+					int bearing = Integer.parseInt(parts[1]);
+					if (c.getCellContent(bearing) == Critter.ALLY) {
+						currentLine = lineJump(parts[2], currentLine, c);
+					} else {
+						currentLine++;
+					}
 				}
-			} else if (action.equals("ifeq")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				int r2 = Integer.parseInt(parts[2].substring(1));
-				int nth = Integer.parseInt((parts[3].substring(1)));
-				if (c.getReg(r1) == c.getReg(r2)) {
-					currentLine = nth;
-				} else {
+				// Register methods start here
+				else if (action.equals("infect")) {
+					if (parts.length == 1) {
+						c.infect();                                  // plain "infect"
+					} else {
+						try {
+							c.infect(Integer.parseInt(parts[1]));    // "infect 5"
+						} catch (NumberFormatException e) {
+							throw new IllegalArgumentException("infect expects a line number, got '" + parts[1] + "'");
+						}
+					}
+					c.setNextCodeLine(currentLine + 1);              // action: save place
+					return;                                          // and end the turn
+				} else if (action.equals("write")) {
+					int reg = parseReg(parts[1]);
+					int value = Integer.parseInt(parts[2]);
+					c.setReg(reg, value);
 					currentLine++;
-				}
-			} else if (action.equals("ifgt")) {
-				int r1 = Integer.parseInt(parts[1].substring(1));
-				int r2 = Integer.parseInt(parts[2].substring(1));
-				int nth = Integer.parseInt((parts[3].substring(1)));
-				if (c.getReg(r1) > c.getReg(r2)) {
-					currentLine = nth;
-				} else {
+				} else if (action.equals("add")) {
+					int r1 = parseReg(parts[1]);
+					int r2 = parseReg(parts[2]);
+					c.setReg(r1, c.getReg(r1) + c.getReg(r2));
 					currentLine++;
+				} else if (action.equals("sub")) {
+					int r1 = parseReg(parts[1]);
+					int r2 = parseReg(parts[2]);
+					c.setReg(r1, c.getReg(r1) - c.getReg(r2));
+					currentLine++;
+
+				} else if (action.equals("inc")) {
+					int r1 = parseReg(parts[1]);
+					c.setReg(r1, c.getReg(r1) + 1);
+					currentLine++;
+				} else if (action.equals("dec")) {
+					int r1 = parseReg(parts[1]);
+					c.setReg(r1, c.getReg(r1) - 1);
+					currentLine++;
+				} else if (action.equals("iflt")) {
+					int r1 = parseReg(parts[1]);
+					int r2 = parseReg(parts[2]);
+					if (c.getReg(r1) < c.getReg(r2)) {
+						currentLine = lineJump(parts[3], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifeq")) {
+					int r1 = parseReg(parts[1]);
+					int r2 = parseReg(parts[2]);
+
+					if (c.getReg(r1) == c.getReg(r2)) {
+						currentLine = lineJump(parts[3], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else if (action.equals("ifgt")) {
+					int r1 = parseReg(parts[1]);
+					int r2 = parseReg(parts[2]);
+					if (c.getReg(r1) > c.getReg(r2)) {
+						currentLine = lineJump(parts[3], currentLine, c);
+					} else {
+						currentLine++;
+					}
+				} else {
+					throw new IllegalArgumentException("unknown instruction '" + action + "'");
 				}
+			} catch(IllegalArgumentException | IndexOutOfBoundsException e) {
+				System.err.println("Line " + currentLine + ": " + e.getMessage());
+				return;
 			}
+			}
+
 		}
-	}
+
 
 	public CritterSpecies loadSpecies(String filename) throws IOException {
 		// Check preconditions for user input
@@ -176,9 +186,12 @@ public class Interpreter implements CritterInterpreter {
 
 			// iterate through all instructions, save them to an ArrayList<String>
 			// exit at the blank line
-			while (!((line = reader.readLine()).isBlank())) {
+			while ((line = reader.readLine()) != null && !line.isBlank()) {
 				code.add(line);
 			}
+		}
+		if(code.size() <= 1) {
+			return null;
 		}
 		return new CritterSpecies(code.get(0), code);
 	}
@@ -186,34 +199,35 @@ public class Interpreter implements CritterInterpreter {
 	// this is a helper method that will translate from the given target line
 	// notation for go to the line number
 	public int lineJump(String target, int currentLine, Critter cri) {
-
-		if (target.charAt(0) == 'r') {
-			String afterR = target.substring(1);
-			int rNum = -1;
-			try {
-				rNum = Integer.parseInt(afterR);
-			} catch (NumberFormatException e) {
-				System.err.println("Text after 'r' was not an integer");
+		try {
+			if (target.charAt(0) == 'r') {
+				return cri.getReg(parseReg(target));
+			} else if (target.charAt(0) == '+' || target.charAt(0) == '-') {
+				return currentLine + Integer.parseInt(target);
+			} else {
+				return Integer.parseInt(target);
 			}
-			return cri.getReg(rNum);
-		} else if (target.charAt(0) == '+' || target.charAt(0) == '-') {
-			int relJump = 0;
-			try {
-				relJump = Integer.parseInt(target);
-			} catch (NumberFormatException e) {
-				System.err.println("Text after '+' or '-' was not an integer");
-			}
-			return (currentLine + relJump);
-
-		} else {
-			int line = currentLine;
-			try {
-				line = Integer.parseInt(target);
-			} catch (NumberFormatException e) {
-				System.err.println("Text after 'r' was not an integer");
-			}
-			return line;
-		}
-
+		} catch (NumberFormatException e) {
+		throw new IllegalArgumentException("bad jump target '" + target + "'");
 	}
+	}
+
+	private int parseReg(String s) {
+		if (!s.startsWith("r")) {
+			throw new IllegalArgumentException("Expected a register, got '" + s + "'");
+		}
+		int n = Integer.parseInt(s.substring(1));
+		if (n < 1 || n > Critter.REGISTERS) {
+			throw new IllegalArgumentException("Register is out of range 1 to 10");
+		}
+		return n;
+	}
+
+
+
+
+
+
+
 }
+
